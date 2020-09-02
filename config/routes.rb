bd07efd41dd741882c78c49257aa0c04cde86585
guguasmon <hutagoazkey@gmail.com>
2020-09-02 14:51:30 +0900

    Twitter認証のルーティングを設置

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
 
   root to: "events#index"
   resources :events
+
+  get '/auth/:provider/callback', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 end
