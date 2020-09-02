427257f04058a852457cb08ef05f170e2f65ced3
guguasmon <hutagoazkey@gmail.com>
2020-09-02 12:36:36 +0900

    userモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_01_033641) do
+ActiveRecord::Schema.define(version: 2020_09_02_032834) do
 
   create_table "events", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
@@ -20,4 +20,25 @@ ActiveRecord::Schema.define(version: 2020_09_01_033641) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "user_event_relations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "event_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_id"], name: "index_user_event_relations_on_event_id"
+    t.index ["user_id"], name: "index_user_event_relations_on_user_id"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "uid", null: false
+    t.string "nickname", null: false
+    t.string "name", null: false
+    t.string "image", null: false
+    t.text "profile"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "user_event_relations", "events"
+  add_foreign_key "user_event_relations", "users"
 end
