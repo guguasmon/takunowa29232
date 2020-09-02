a938d9121bd61a0e60461cb9bbceb27796b72812
guguasmon <hutagoazkey@gmail.com>
2020-09-02 16:12:21 +0900

    ログインログアウト処理の実装を確認

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user_data = request.env['omniauth.auth']
     user = User.find_by(uid: user_data[:uid])
     if user
-      log_in user
+      log_in(user)
       flash[:success] = 'ログインしました'
       redirect_to root_url
     else
@@ -18,7 +18,7 @@ class SessionsController < ApplicationController
         image: user_data[:info][:image],
       )
       if new_user.save
-        log_in new_user
+        log_in(new_user)
         flash[:success] = 'ユーザー登録成功'
       else
         flash[:danger] = '予期せぬエラーが発生しました'
@@ -27,10 +27,33 @@ class SessionsController < ApplicationController
     end
   end
 
-
   def destroy
     log_out if logged_in?
     flash[:success] = 'ログアウトしました'
     redirect_to root_url
+  end
+
+  private
+  # 渡されたユーザーでログインする
+  def log_in(user)
+    session[:uid] = user.uid
+  end
+
+  # 現在ログイン中のユーザーを返す (いる場合)
+  def current_user
+    if session[:uid]
+      @current_user ||= User.find_by(uid: session[:uid])
+    end
+  end
+
+  # ユーザーがログインしていればtrue、その他ならfalseを返す
+  def logged_in?
+    !current_user.nil?
+  end
+
+  # 現在のユーザーをログアウトする
+  def log_out
+    session.delete(:uid)
+    @current_user = nil
   end
 end
