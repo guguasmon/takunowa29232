368a3f91393b02358f96a41e8ebffbcf0e26aa68
guguasmon <hutagoazkey@gmail.com>
2020-09-02 17:09:01 +0900

    userモデルのバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
 
   has_many :user_event_relations
   has_many :events, through: :user_event_relations
+
+  with_options presence: true do
+    validates :uid
+    validates :nickname
+    validates :name
+    validates :image
+  end
 end
